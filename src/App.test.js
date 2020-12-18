dddc05d78d2dfd89dd70e45dc9a62f165948f9a0
kumarAjayt <ajay05.tiwary@gmail.com>
2020-12-18 05:30:28 +0530

    added travis configuration with app test update

--- src/App.test.js
+++ src/App.test.js
@@ -7,10 +7,6 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-it('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
 
 
